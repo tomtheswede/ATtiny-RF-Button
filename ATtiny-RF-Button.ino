d761a187f61d8c37ed2762468e9376645791fd6b
Thomas Friberg <tomtheswede@gmail.com>
2016-10-09 22:05:35 +0800

    Change to interrupt mode

--- ATtiny-RF-Button.ino
+++ ATtiny-RF-Button.ino
@@ -1,11 +1,19 @@
+/*  
+ *   For sending RF signals from battery operated ATtiny85
+ *   Code by Thomas Friberg (https://github.com/tomtheswede)
+ *   Updated 9/10/2016
+ */
 
-//#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit)) //OR
-//#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit)) //AND
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit)) //OR
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit)) //AND
 
-const unsigned int butPin = 3;
+//Interrupt variables
 volatile bool triggered=false;
+volatile bool buttonState=false;
+volatile const unsigned int butPin = 3; //button pin
+volatile unsigned long lastTrigger;
 
-int sendPin = 2;
+const unsigned int sendPin = 2; //RF pin
 int butState1=LOW;
 int butState2=HIGH;
 int devID = 4; //Reads as "3" corresponds with BTN type
@@ -18,28 +26,32 @@ void setup() {
   pinMode(butPin,INPUT_PULLUP);
   pinMode(sendPin,OUTPUT);
   delay(20);
+  lastTrigger=millis();
 
   //Start interupts
-  //sbi(GIMSK,PCIE); //Turn on interrupt
-  //sbi(PCMSK,PCINT1); //set pin affected by interupt
+  sbi(GIMSK,PCIE); //Turn on interrupt
+  sbi(PCMSK,PCINT3); //set pin affected by interupt
 }
 
 void loop() {
   butState2 = butState1;
   butState1 = digitalRead(butPin);
 
-  if (butState2==HIGH && butState1==LOW) {
+  if (triggered) {
+    delay(10);
     encodeMessage();
+    triggered=false;
     delay(30);
   }
   delay(0);
 }
 
-//ISR(PCINT0_vect) {
-//  if (digitalRead(butPin)==LOW && !triggered) {
-//    triggered=true;
-//  }
-//}
+ISR(PCINT0_vect) {
+  buttonState=!digitalRead(butPin);
+  if (buttonState && !triggered && (millis()-lastTrigger>200 || millis()<lastTrigger)) { //includes 200ms debounce
+    triggered=true;
+  }
+}
 
 
 void encodeMessage() { //message should read 3,2,f5
@@ -48,11 +60,11 @@ void encodeMessage() { //message should read 3,2,f5
   encodeNumber(devID); //code 4, reads as 3
   encodeNumber(1); //comma
   encodeNumber(sensorName1); //code 3 reads as 2
-  encodeNumber(1); //comma
+  //encodeNumber(1); //comma
   //encodeNumber(4); //3
   //encodeNumber(12);//full stop
-  encodeNumber(13);
-  encodeNumber(6); //5
+  //encodeNumber(13);
+  //encodeNumber(6); //5
   encodeNumber(14);//End
 }
 
