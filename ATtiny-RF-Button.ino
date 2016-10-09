2d8aac487fbbec2a35418aa48d87a93ae00a5c61
Thomas Friberg <tomtheswede@gmail.com>
2016-10-09 10:09:02 +0800

    Update to reference table

--- ATtiny-RF-Button.ino
+++ ATtiny-RF-Button.ino
@@ -1,41 +1,67 @@
-int code[14] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
-String out[14] = {",","1","2","3","4","5","6","7","8","9","0",".","e","s"};
-int sendPin = 0;
+
+//#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit)) //OR
+//#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit)) //AND
+
+const unsigned int butPin = 3;
+volatile bool triggered=false;
+
+int sendPin = 2;
+int butState1=LOW;
+int butState2=HIGH;
+int devID = 4; //Reads as "3" corresponds with BTN type
+int sensorName1 = 3; //Reads as 2
+long lastPress=1;
+
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(butPin,INPUT_PULLUP);
   pinMode(sendPin,OUTPUT);
+  delay(20);
+
+  //Start interupts
+  //sbi(GIMSK,PCIE); //Turn on interrupt
+  //sbi(PCMSK,PCINT1); //set pin affected by interupt
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  encodeMessage();
-  encodeMessage();
-  encodeMessage();
-  encodeMessage();
-  delay(15);
+  butState2 = butState1;
+  butState1 = digitalRead(butPin);
+
+  if (butState2==HIGH && butState1==LOW) {
+    encodeMessage();
+    delay(30);
+  }
+  delay(0);
 }
 
-void encodeMessage() {
-  encodeNumber(14); //Start
-  encodeNumber(3); //2
+//ISR(PCINT0_vect) {
+//  if (digitalRead(butPin)==LOW && !triggered) {
+//    triggered=true;
+//  }
+//}
+
+
+void encodeMessage() { //message should read 3,2,f5
+  encodeNumber(2); //A signal to level the receiver/prep for start signal
+  encodeNumber(15); //Start
+  encodeNumber(devID); //code 4, reads as 3
   encodeNumber(1); //comma
-  encodeNumber(2); //1
+  encodeNumber(sensorName1); //code 3 reads as 2
   encodeNumber(1); //comma
-  encodeNumber(4); //3
-  encodeNumber(12);//full stop
+  //encodeNumber(4); //3
+  //encodeNumber(12);//full stop
+  encodeNumber(13);
   encodeNumber(6); //5
-  encodeNumber(13);//End
-    
-  delayMicroseconds(500);
+  encodeNumber(14);//End
 }
 
 void encodeNumber(int num) {
   for (int i=0; i<num; i++) {
     digitalWrite(sendPin,HIGH);
-    delayMicroseconds(10);
+    delayMicroseconds(430);
     digitalWrite(sendPin,LOW);
-    delayMicroseconds(10);
+    delayMicroseconds(400);
   }
-  delayMicroseconds(300);
+  delayMicroseconds(900);
 }
